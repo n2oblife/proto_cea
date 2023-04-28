86808e52dae150fd47c8d90cc6e4631a87b0946d
n2oblife <zackanit@gmail.com>
2023-04-28 15:02:31 +0200

    xlm ok

--- tests/training/test_Nllb_tok.py
+++ tests/training/test_Nllb_tok.py
@@ -6,13 +6,13 @@ import trankit.models.base_models as base_models
 import trankit.config as conf
 
 main_loc_path = '/home/zk274707/Projet/'
-main_factoryAI_path = '/home/users/zkanit/'
+main_factoryAI_path = '/home/users/zkanit/proto_utils/'
 
 print(torch.cuda.is_available())
 
 #tokenizer = NllbTokenizer.from_pretrained("facebook/nllb-200-distilled-600M")
-tok_path = '../save_dir/HG/nllb_tok.pt'
-tokenizer = torch.load(tok_path)
+tok_path_factoryAI = '../proto_utils/save_dir/HG/nllb_tok.pt'
+tokenizer = torch.load(tok_path_factoryAI)
 
 print(tokenizer.model_max_length)
 
@@ -26,10 +26,10 @@ docu = tokenizer(batch_sentences_list, padding = 'max_length')
 print(docu.keys())
 
 configuration = conf.Config()
-configuration._cache_dir = './cache/trankit/tpPipeline'
+configuration._cache_dir = '../proto_utils/cache/trankit/tpPipeline'
 bse_mdl = base_models.Base_Model(config=configuration, task_name='posdep')
 
-pipe = Pipeline('auto',gpu=False)
+pipe = Pipeline('auto') # Rq : 'gpu = False / dans le cas du taff sur cpu meme si ca marche pas
 
 input_hidden_arc_dim = 128
 UD_n_labels = 37 
@@ -44,7 +44,7 @@ DB = base_models.Deep_Biaffine(tokenizer.model_max_length, # input dimmension is
 training_config={
     #'category': 'customized-mwt-ner', # pipeline category
     'task': 'posdep', # task name
-    'save_dir': '../save_dir', # directory for saving trained model
+    'save_dir': '../proto_utils/save_dir', # directory for saving trained model
     #'gpu' : False,
     'train_conllu_fpath': main_factoryAI_path+'datasets/ud-treebanks-v2.10-trainable/UD_English-EWT/en_ewt-ud-train.conllu', # annotations file in CONLLU format  for training
     'dev_conllu_fpath': main_factoryAI_path+'datasets/ud-treebanks-v2.10-trainable/UD_English-EWT/en_ewt-ud-dev.conllu' # annotations file in CONLLU format for development
